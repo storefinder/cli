e93835e72f0503f33dc7fd87cc1c941be604e79e
Ram Gopinathan <rprakashg@gmail.com>
2018-12-22 19:28:57 -0800

    Removing build.sh as its no longer needed

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t storefinder/cli . -f Dockerfile && \
-docker create --name storefinder-cli storefinder/cli && \
-docker cp storefinder-cli:./storefinder . && \
-docker rm -f storefinder-cli
